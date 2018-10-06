b8575606d320ee66b3888d16d5a212acdad2d1f9
Content Team <contentteam@UE-M-NOTE-0101.local>
2018-10-06 10:26:49 +0530

    hello

---
+++ src/main/java/upgrad/HelloWorldServlet.java
@@ -0,0 +1,28 @@
+package upgrad;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/helloworld")
+public class HelloWorldServlet extends HttpServlet {
+
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        PrintWriter printWriter = response.getWriter();
+        printWriter.print("<!Doctype html>");
+        printWriter.print("<html>");
+        printWriter.print("<head>");
+        printWriter.print("<title>Hello World Servlet!</title>");
+        printWriter.print("</head>");
+        printWriter.print("<body>");
+        printWriter.print("<p>Hello World!</p>");
+        printWriter.print("</body>");
+        printWriter.print("</html>");
+        printWriter.close();
+    }
+
+}
